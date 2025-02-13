0bcae2db7bd1ce6a56ceb77e5fe654b07a17f023
jdmedlock <jdmedlock@gmail.com>
2025-02-13 09:01:55 -0600

    Refactor: Adjust indentation

--- src/consoleCount.js
+++ src/consoleCount.js
@@ -1,10 +1,10 @@
 // console.count
 function greet(user) {
-    console.count(user)
-    return `hi ${user}`
-  }
-    
-  greet(`Andres`)
-  greet(`Dinesh`)
-  greet('Andres')
-  console.count('Andres')
+  console.count(user)
+  return `hi ${user}`
+}
+  
+greet(`Andres`)
+greet(`Dinesh`)
+greet('Andres')
+console.count('Andres')
