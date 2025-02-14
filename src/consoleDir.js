b0278cac75eb9746864651ab3cbbcb3236904655
jdmedlock <jdmedlock@gmail.com>
2025-02-14 09:33:05 -0600

    Feature: Update examples to add different conditions

--- src/consoleDir.js
+++ src/consoleDir.js
@@ -1,3 +1,8 @@
 // console.dir
 const person = {name: 'Jim', role: 'Developer'}
 console.dir(person)
+console.dir(`person:`, person) // dir only accepts a single parameter
+
+console.log(`\n\nperson: `, person)
+console.log(`person: ${ JSON.parse(JSON.stringify(person)) }`) // What???
+console.log(`person: `, JSON.parse(JSON.stringify(person)))
